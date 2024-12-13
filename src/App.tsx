ef1b90ba527ba249e4add54968f4301d6f681069
Le Trung Nguyen <nguyenc2300110@student.ctu.edu.vn>
2024-12-13 21:43:45 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Helloworld from "./components/Helloworld";
+
+function App() {
+  return (
+    <div className={"app"}>
+      <Helloworld/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Helloworld.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Helloworld(){
+
+    return(
+        <div>
+            <h1>Hello world</h1>
+        </div>
+    )
+}
+
+export default Helloworld

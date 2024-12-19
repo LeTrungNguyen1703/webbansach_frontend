360ac5ce833284cdfbd25eb6a3c409b2b8ece5e2
Le Trung Nguyen <nguyenc2300110@student.ctu.edu.vn>
2024-12-19 19:31:29 +0700

    Tao Footer.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 import Navbar from "./layouts/header-footer/Navbar";
+import Footer from "./layouts/header-footer/Footer";
 
 function App() {
     return (
-       <Navbar/>
+        <>
+            <Navbar/>
+            <Footer/>
+        </>
+
     )
 }
 

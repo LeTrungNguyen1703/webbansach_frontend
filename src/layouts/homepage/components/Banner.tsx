33cac3f0718272e356af55e4bca6998327d93cf4
Le Trung Nguyen <nguyenc2300110@student.ctu.edu.vn>
2024-12-19 19:58:44 +0700

    Banner.tsx (chua co href cho button)

--- src/layouts/homepage/components/Banner.tsx
+++ src/layouts/homepage/components/Banner.tsx
@@ -3,7 +3,21 @@ import React from "react";
 export function Banner(){
     return (
         <>
+            <div className={"p-5 mb-4 bg-dark"}>
+                <div className={"container-fluid py-5 text-white d-flex " +
+                    "justify-content-center align-items-center"}>
+                    <div>
+                        <h3 className={"display-5"}>
+                           <p className={"fw-bold"}>
+                               Đọc sách chính là hộ chiếu <br/>
+                               cho vô số cuộc phiêu
+                           </p>
+                            <button className={"btn btn-primary float-end mt-2"}>Khám phá gian hàng sách tại đây</button>
+                        </h3>
+                    </div>
+                </div>
 
+            </div>
         </>
     )
 }

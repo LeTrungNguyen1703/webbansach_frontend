34cc48f42f125b8da4c96a51d602dbdcbfd2d282
Le Trung Nguyen <nguyenc2300110@student.ctu.edu.vn>
2024-12-21 16:38:27 +0700

    Tao product trong HomePage.tsx

--- src/layouts/homepage/HomePage.tsx
+++ src/layouts/homepage/HomePage.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import {Banner} from "./components/Banner";
 import {Carousel} from "./components/Carousel";
+import List from "../product/List";
 
 export function HomePage(){
     return (
         <>
             <Banner/>
             <Carousel/>
+            <List/>
         </>
     )
 }
